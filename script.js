e1af774cbd527631a5aa0489793bc6dc59160556
Elizabeth Karr <e.karr93@icloud.com>
2023-05-30 11:03:21 -0500

    code Cell module

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function Gameboard() {
   const rows = 3;
   const columns = 3;
   const board = [];
+}
 
-  
+function Cell() {
+  let value = ' ';
+
+  // accept a player's token to change the value of the cell
+  const addToken = (player) => {
+    value = player;
+  };
+
+  // get current value of cell
+  const getValue = () => value;
+
+  return { addToken, getValue };
 }
