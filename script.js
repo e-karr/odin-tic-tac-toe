076bf5d9467ce0fc9a120fe1d13bbd7c6f07d395
Elizabeth Karr <e.karr93@icloud.com>
2023-05-30 11:09:06 -0500

    create gameboard array

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function Gameboard() {
   const rows = 3;
   const columns = 3;
   const board = [];
+
+  // create 2d array that will represent state of the gameboard
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  console.log(board);
 }
 
 function Cell() {
